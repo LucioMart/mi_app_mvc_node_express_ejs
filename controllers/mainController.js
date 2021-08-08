46e0fbe243d5d6bf5e879dbbd83568c2343bc4ce
LucioMart <luchoweb15@gmail.com>
2021-08-08 18:17:08 -0300

    Lectura del json en el mainController, y mudularización n del html, para mostrar los datos dinamicamente.

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,15 +1,23 @@
 const { render } = require('ejs')
-const express = require('express')
-const router = express.Router()
+// const express = require('express')
+// const router = express.Router()
 const path = require('path')
+const fs = require('fs')
+
+const platosFilePath = path.join(__dirname, '../data/platosDataBase.json')
+const platos = JSON.parse(fs.readFileSync(platosFilePath, 'utf-8'))
 
 
 const controlador = {
     home : (req, res) => {
-        return res.render('index')
+        return res.render('index', {
+            platos
+        })
     },
     detalleMenu : (req, res) => {
-        return res.render('detalleMenu')
+        return res.render('detalleMenu', {
+            platos
+        })
     }
 }
 
